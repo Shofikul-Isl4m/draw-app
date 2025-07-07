f589a2dd03315b540159a5b783cd83bd1e0b77d1
Shofikul islam <shofikul.islam23102023@gmail.com>
2025-07-07 08:17:54 +0600

    update middleware

--- apps/http-backend/src/middleware/middleware.ts
+++ apps/http-backend/src/middleware/middleware.ts
@@ -2,24 +2,37 @@ import { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
 export function middleware(req: Request, res: Response, next: NextFunction) {
-  const token = req.headers["authorization"];
-  if (!token) {
-    throw new Error("no token found");
+  let token: string | undefined;
+  if ((req.cookies && req.cookies, jwt)) {
+    token = req.cookies["jwt"];
+  } else if (req.headers.authorization?.startsWith("Bearer ")) {
+    token = req.headers.authorization.split(" ")[1];
   }
 
-  if (!process.env.JWT_SECRET) {
-    throw new Error(
-      " JWT_SECRET is not defined. Please check your environment configuration."
-    );
+  if (!token) {
+    return res.status(401).json({
+      message: " Authentication required",
+    });
   }
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  console.log(decoded);
-  if (typeof decoded === "object" && "userId" in decoded) {
-    req.userid = decoded.userId;
-    next();
-  } else {
-    res.status(404).json({
-      message: "unathorized",
+  try {
+    if (!process.env.JWT_SECRET) {
+      throw new Error(
+        " JWT_SECRET is not defined. Please check your environment configuration."
+      );
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+    if (typeof decoded === "object" && "userId" in decoded) {
+      req.userid = decoded.userId;
+      next();
+    } else {
+      res.status(401).json({
+        message: "invalid token playload",
+      });
+    }
+  } catch (error) {
+    res.status(401).json({
+      message: "invalid or expired token",
     });
   }
 }
